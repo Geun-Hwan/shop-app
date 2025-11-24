cc18b8cee58c56ce3771881691bc88e1040680eb
Geun-Hwan <rmsghks1026@gmail.com>
2025-11-24 14:47:31 +0900

    refactor : member 도메인 분리

--- src/main/java/com/example/shop/member/infrastructure/MemberJpaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.shop.member.infrastructure;
-
-import com.example.shop.member.domain.Member;
-import java.util.UUID;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberJpaRepository extends JpaRepository<Member, UUID> {
-
-}
